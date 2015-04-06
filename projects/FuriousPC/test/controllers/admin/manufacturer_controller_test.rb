a6a6bc3a6ab171745120b3d4e4fcbe09095b98e6
Ramón Ramírez González <rrgonzalez1992@gmail.com>
2015-04-06 17:36:10 +0200

    Agregado tests de CRUD Manufacturer

--- projects/FuriousPC/test/controllers/admin/manufacturer_controller_test.rb
+++ projects/FuriousPC/test/controllers/admin/manufacturer_controller_test.rb
@@ -1,39 +1,72 @@
-require 'test_helper'
+reqquire File.dirname(__FILE__) + '/../test_helper'
 
 class Admin::ManufacturerControllerTest < ActionController::TestCase
-  test "should get new" do
+  test "new" do
     get :new
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
+  test "create" do
+    num_manufacturers = Manufacturer.count
+    post :create, :manufacturer => {:name => 'Prueba', :phone => '987654321', 
+    	  :tlf => '123456789', :direction: 'Direccion Prueba'}
+    	  assert_response :redirect
+    	  assert_redirected_to :action => 'index'
+    	  assert_equal num_manufacturers +1, Manufacturer.count
   end
 
-  test "should get edit" do
-    get :edit
-    assert_response :success
+  test "edit" do
+    get :edit, :id => 1
+    assert_tag :tag => 'input', :attributes => {:name => 'manufacturer [name]', :value => 'Prueba Edit'}
+    assert_tag :tag => 'input', :attributes => {:name => 'manufacturer [tlf]', :value => '111111111'}
+    assert_tag :tag => 'input', :attributes => {:name => 'manufacturer [phone]', :value => '222222222'}
+    assert_tag :tag => 'input', :attributes => {:name => 'manufacturer [direction]', :value => 'Prueba Direccion Edit'}
   end
 
-  test "should get update" do
-    get :update
-    assert_response :success
+  test "update" do
+    post :update, :id => 1, :manufacturer => {:name => 'Prueba Update',
+    										  :tlf => '333333333'
+    										  :phone => '444444444'
+    										  :direction => 'Prueba Direccion Update'}
+    assert_response :redirect
+    assert_redirected_to :action => 'show', :id => 1
+    assert_equal 'Prueba Update', Manufacturer.find(1).name
+    assert_equal '333333333', Manufacturer.find(1).tlf
+    assert_equal '444444444', Manufacturer.find(1).phone
+    assert_equal 'Prueba Direccion Update', Manufacturer.find(1).direction
   end
 
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
+  test "destroy" do
+    assert_difference(Manufacturer, :count, -1) do
+    	post :destroy, :id => 1
+    	assert_equal flash[:notice], 'Se ha borrado el fabricante'
+    	assert_response :redirect
+    	assert_redirected_to :action => 'index'
+    	get :index
+    	assert_response :success
+    	assert_tag :tag => 'div', :attributes => {:id => 'notice'},
+                 :content => 'Se ha borrado el fabricante.'
+    end
   end
 
-  test "should get show" do
-    get :show
+  test "show" do
+    get :show, :id => 1
     assert_response :success
+    assert_template 'manufacturer/show'
+    assert_tag "h1", :content => Manufacturer.find(1).name
+    assert_not_nil assigns(:manufacturer)
+    assert assigns(:manufacturer).valid?
   end
 
-  test "should get index" do
+  test "index" do
     get :index
     assert_response :success
+    assert_tag :tag=> 'table', :children => { :count => Manufacturer.count + 1, :only => {:tag => 'tr'} }
+  	Manufacturer.find_each do |m|
+  		assert_tag :tag => 'td', :content => m.name
+  		assert_tag :tag => 'td', :content => m.tlf
+  		assert_tag :tag => 'td', :content => m.phone
+  		assert_tag :tag => 'td', :content => m.direction
+  	end
   end
-
 end
--- projects/FuriousPC/test/models/manufacturer_test.rb
+++ projects/FuriousPC/test/models/manufacturer_test.rb
@@ -1,7 +1,21 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class ManufacturerTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "failing_create" do
+  	manufacturer = Manufacturer.new
+  	assert_equal false, manufacturer.save
+  end
+
+  test "create" do
+  	manufacturer = Manufacturer.new(
+  		:name = 'Prueba',
+  		:tlf = '900000000',
+  		:phone = '000000009',
+  		:direction = 'Direccion prueba'
+  		)
+  	assert manufacturer.save
+  end
 end
