c32af10b80047f8099d049c378ea6568173a46c7
wrenq <enrique.arauz@gmail.com>
2015-04-20 19:15:58 +0200

    Mejoras en tests de item II

--- projects/FuriousPC/test/models/item_test.rb
+++ projects/FuriousPC/test/models/item_test.rb
@@ -29,6 +29,7 @@ class ItemTest < ActiveSupport::TestCase
   	item = Item.new(
   		:name => 'Test creacion procesador',
         :description => 'Es un procesador de test',
+        :tipo => 'Processor'
         :price => '800€',
         :weight => '1kg',
         :dimensions => '1x1x1cm',
