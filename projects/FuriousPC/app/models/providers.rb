9fc24e3486c0ed7359ccd77746f9a5d668e05fbd
wrenq <enrique.arauz@gmail.com>
2015-04-20 20:22:02 +0200

    Tabla Proveedores y modificacion de tests

--- projects/FuriousPC/app/models/providers.rb
+++ projects/FuriousPC/app/models/providers.rb
@@ -1,2 +1,4 @@
 class Providers < ActiveRecord::Base
+	has_and_belongs_to_many :item
+	validates_presence_of :name, :tlf, :direction, :phone
 end
--- projects/FuriousPC/test/models/item_test.rb
+++ projects/FuriousPC/test/models/item_test.rb
@@ -41,4 +41,33 @@ class ItemTest < ActiveSupport::TestCase
   		)
   	assert item.save
   end
+
+  test "test_has_many_and_belongs_to_mapping" do
+    fabricante = Manufacturer.find(1)
+    assert_equal 1, fabricante.item.size
+    componente = Item.new(
+        :name => 'Test creacion procesador',
+        :description => 'Es un procesador de test',
+        :tipo => 'Processor',
+        :price => '800€',
+        :weight => '1kg',
+        :dimensions => '1x1x1cm',
+        :id_manufacturer => 1,
+        :socket => 'FM3',
+        :TDP => '50W',
+        :number_cores => 4,
+        :core_frequency => 2,
+
+
+      :providers => [Providers.find(1),
+      Providers.find(2)]
+
+      )
+    fabricante.books << book
+    fabricante.reload
+    item.reload
+    assert_equal 3, fabricante.item.size
+    assert_equal 1, item.manufacturer.id
+  end
+
 end
