265b6e0a0b0e991e4cce3e7f8b9904bd51c917fa
Ramón Ramírez González <rrgonzalez1992@gmail.com>
2015-04-27 18:25:46 +0200

    Arreglado test modelo item

--- projects/FuriousPC/test/models/item_test.rb
+++ projects/FuriousPC/test/models/item_test.rb
@@ -1,19 +1,43 @@
 require 'test_helper'
 
-class Item < ActiveRecord::Base
- 	belongs_to :manufacturer
-+	has_and_belongs_to_many :providers
- 
- 	
- 	validates_length_of :name, :in => 1..255
-@@ -10,6 +11,9 @@ class Item < ActiveRecord::Base
- 	validates_presence_of :weight
- 	validates_presence_of :price
- 	validates_presence_of :id_manufacturer
-+	validates_presence_of :tipo
-+	validates_presence_of :name
-+	validates_presence_of :description
- end
- 
- def name
+class ItemTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  test "failing_create" do
+  	item = Item.new
+  	assert_equal false, item.save
+	assert item.errors[:name]
+	assert item.errors[:description]
+	assert item.errors[:tipo]
+	assert item.errors[:price]
+	assert item.errors[:weight]
+	assert item.errors[:dimensions]
+	assert item.errors[:id_item]
+	assert item.errors[:socket]
+	assert item.errors[:TDP]
+	assert item.errors[:number_cores]
+	assert item.errors[:core_frequency]
+	assert item.errors[:factor]
+	assert item.errors[:video_memory]
+	assert item.errors[:pci_version]
+	assert item.errors[:watts]
 
+  end
+
+  test "create" do
+  	item = Item.new(
+  		:name => 'Test creacion procesador',
+        :description => 'Es un procesador de test',
+        :price => '800€',
+        :weight => '1kg',
+        :dimensions => '1x1x1cm',
+        :id_manufacturer => 2,
+        :socket => 'FM3',
+        :TDP => '50W',
+        :number_cores => 4,
+        :core_frequency => 2
+  		)
+  	assert item.save
+  end
+end
